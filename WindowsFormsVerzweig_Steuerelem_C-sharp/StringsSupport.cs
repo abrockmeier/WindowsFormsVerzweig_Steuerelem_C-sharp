876321c7d23a3501d8a1dc063d77504543118960
abrockmeier <alexander.brockmeier@ambicotech.com>
2019-01-18 02:11:18 +0100

    v2.6
    Upgrade! (faster better stronger)

--- WindowsFormsVerzweig_Steuerelem_C-sharp/StringsSupport.cs
+++ WindowsFormsVerzweig_Steuerelem_C-sharp/StringsSupport.cs
@@ -71,85 +71,60 @@ namespace WindowsFormsVerzweig_Steuerelem_C_sharp
 			if (Length >= 75)
 			{
 				Output = "You have crossed the Number of 75 entered chars! xD";
-				if ((Length > 51)&&(Length<75))
-				{
-					for (int i = 0; i + Odd  < (Length / 3) ; i++)
-					{
-						Output += (Input[i] + System.Environment.NewLine);
-					}
-
-					for (int i = (Length - Length/3); i <= Length/3*2; i++)
-					{
-						Output2 += (Input[i] + System.Environment.NewLine);
-					}
-					for (int i =  Length/3*2 ; i < length - Even; i++)
-					{
-						Output3 += (Input[i] + System.Environment.NewLine);
-					
-					}
-				
-				if ((Length > 25)&&(Length <=50))
-				{
-					for (int i = 0; i < (Length / 2 + Odd) ; i++)
-					{
-						Output += (Input[i] + System.Environment.NewLine);
-					}
-
-					for (int i = (Length - Length/2); i <= Length - Even; i++)
-					{
-						Output2 += (Input[i] + System.Environment.NewLine);
-					}
-				
-				}
-				}
 			}
-			else 
+			else if (Length > 50)
+			{
+				for (int i = 0; i < (Length / 3  + Odd); i++)
+				{
+					Output += (Input[i] + System.Environment.NewLine);
+				}
+				for (int j = Length / 3+Odd; j < (Length / 3+ Odd) * 2; j++)
+				{
+					Output2 += (Input[j] + System.Environment.NewLine);
+				}
+				for (int k = Length / 3 * 2 +2*Odd; k  < length ; k++)
+				{
+					Output3 += (Input[k] + System.Environment.NewLine);
+
+				}
+			}	
+			else if (Length > 25)
+			{
+				for (int i = 0; i < (Length / 2 + Odd) ; i++)
+				{
+					Output += (Input[i] + System.Environment.NewLine);
+				}
+
+				for (int i = (Length - Length/2); i < Length ; i++)
+				{
+					Output2 += (Input[i] + System.Environment.NewLine);
+				}
+				
+			}
+			else if (Length <= 25)
 			{
 				foreach (char c in Input)
 				{
 					Output += (c + System.Environment.NewLine);
 				}
+			
 			}
-			 
 		}
-			public void OddOrEven()
-			{
+
+
+		public void OddOrEven()
+		{
 			if (Length % 2 == 0)
+			{
 				Even = 1;
-			else Odd = 1;
+				Odd = 0;
+			}
+			else
+			{
+				Odd = 1;
+				Even = 0;
 			}
 
+		}
 	}
 }
-		/* old version with string builder but only one Label,...
-		public void String_toChar()
-		{
-			Output = "label:" + System.Environment.NewLine;
-			foreach (char c in Input)
-			{
-				if (Length <= 25)
-					Output += (c + System.Environment.NewLine);
-				if (Length > 25)
-				{
-					
-					for (int i = Length; i % 2 != 0; i--)
-					{
-						sb.Append(c + "");
-					}
-					for (int i = Length; i % 2 == 0; i--)
-					{
-						sb.Append(c);
-						sb.Append(System.Environment.NewLine);
-					}
-					
-				}
-				else
-				{
-					Output = "Please feed me with less Input!";
-					break;
-				}
-			}
-			Output += sb;
-			Input = "";
-		} 
-		*/
